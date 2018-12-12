cefdc8838312392d30dc44f3c3a0fd4be86c5809
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2018-12-12 14:34:26 +0100

    Fixing access to tokens.

--- tokens/plugins.py
+++ tokens/plugins.py
@@ -14,4 +14,4 @@ class ProjectAppPlugin(SiteAppPluginPoint):
     description = "API Token Management"
 
     #: Required permission for accessing the app
-    app_permission = "tokens.view_data"
+    app_permission = "tokens.access"
