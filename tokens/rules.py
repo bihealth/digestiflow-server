52194ebcf72d32a567f3734a3aeca6dee6f2e78e
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2019-01-10 09:43:48 +0100

    Fixing access to tokens app.

--- tokens/rules.py
+++ tokens/rules.py
@@ -7,8 +7,5 @@ from projectroles import rules as pr_rules
 # ``tplems.access`` -- Access to the tokens app.
 rules.add_perm(
     "tokens.access",
-    rules.is_superuser
-    | pr_rules.is_project_owner
-    | pr_rules.is_project_delegate
-    | pr_rules.is_project_contributor,
+    rules.is_active
 )
