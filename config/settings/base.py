8c6628a077e8927349bc7c8b5933e82b3927bf21
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2019-01-14 09:50:02 +0100

    Entering admin email.

--- config/settings/base.py
+++ config/settings/base.py
@@ -122,7 +122,7 @@ EMAIL_SUBJECT_PREFIX = env("EMAIL_SUBJECT_PREFIX", default="")
 # MANAGER CONFIGURATION
 # ------------------------------------------------------------------------------
 # TODO: add your information here
-ADMINS = [("""YOUR NAME HERE""", "YOUR.EMAIL@example.com")]
+ADMINS = [("""Manuel Holtgrewe""", "manuel.holgrewe@bihealth.de")]
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
